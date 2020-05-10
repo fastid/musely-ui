4e3b31f77b8548b921c2fc3c2580f949c4ea0570
wangqian <wangqian@rd.tuan800.com>
2020-05-11 02:46:34 +0800

    fix package message-box import file path

--- packages/message-box/index.ts
+++ packages/message-box/index.ts
@@ -2,8 +2,8 @@
  * @Author: Victor wang
  * @Date: 2020-05-06 23:37:09
  * @LastEditors: Victor.wang
- * @LastEditTime: 2020-05-11 01:35:09
+ * @LastEditTime: 2020-05-11 02:45:43
  * @Description:
  */
-import MessageBox from './src/index copy'
+import MessageBox from './src/index'
 export default MessageBox
